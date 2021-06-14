7c7ec2f70809a0467c155758f252cb04c132c9e0
suanmiao <19983194361@189.cn>
2021-06-14 21:25:56 +0800

    add read config feature

--- utils/common.py
+++ utils/common.py
@@ -1,6 +1,18 @@
-import os
 import time
-from utils.path import REPORT_DIR
+import yaml
+from utils.path import REPORT_DIR, CONFIG_DIR
+
+
+def get_config(section=None):
+    """
+    从配置文件中读取详细配置信息
+    :param section: 配置项名称
+    :return: 配置项内容
+    """
+    conf_file = CONFIG_DIR + '\\' + 'conf.yaml'
+    with open(file=conf_file, mode='r', encoding='utf-8') as f:
+        conf_content = yaml.load(stream=f, Loader=yaml.FullLoader)
+        return conf_content[section]
 
 
 def get_current_time():
--- utils/path.py
+++ utils/path.py
@@ -5,3 +5,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # 报告路径
 REPORT_DIR = os.path.join(BASE_DIR, r'reports')
+
+# 配置路径
+CONFIG_DIR = os.path.join(BASE_DIR, r'configs')
